42fd59fd4f0b40fb75572e70c8c4c13617639a62
Andrea Rossini <rossini.andrea.lc@gmail.com>
2026-04-20 22:21:29 +0200

    chore: license headers

--- tool-template.sh
+++ tool-template.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# This file is part of bash-agent.
+# Copyright (C) 2026 Andrea Rossini
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # This is a tool template file. Copy it under ./tools or your subagent folder
 # when you want to create a new tool. I suggest the file name to be short,
 # meaningful, and with no extension.
